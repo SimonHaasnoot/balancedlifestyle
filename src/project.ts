4b7ef91892318c1cf02f7e422b0e523e0cf5dee5
Simon Haasnoot <simonhaasnoot@hotmail.com>
2024-04-30 21:41:59 +0200

    Change address

--- src/project.ts
+++ src/project.ts
@@ -4,7 +4,7 @@ export enum projectVariables {
     COMPANY_NAME = 'Balanced Lifestyle',
     COMPANY_ONELINER = 'Waar balans centraal staat',
     COMPANY_DESCRIPTION = "Balanced Lifestyle is gespecialiseerd in het optimaliseren van de leefstijl door balans te creëren in beweging, voeding en rust. Regio Zeist en Utrecht.",
-    COMPANY_ADDRESS = 'Korte Steynlaan 44 – 3701 EX Zeist',
+    COMPANY_ADDRESS = 'Korte Steynlaan 22 – 3701 EX Zeist',
     COMPANY_PHONE = '0637199776',
     COMPANY_PHONE2 = '0619238149',
     COMPANY_EMAIL = 'info@balancedlifestyle.nl',
