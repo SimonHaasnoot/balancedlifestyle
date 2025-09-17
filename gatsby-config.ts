ae35c7eb4dc066e8f5692fea6b1acdca87e47088
Simon Haasnoot <simonhaasnoot@hotmail.com>
2025-09-17 10:14:37 +0200

    update

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,10 +9,8 @@ module.exports = {
         `gatsby-plugin-image`,
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
-        'gatsby-plugin-netlify',
         'gatsby-plugin-sitemap',
         'gatsby-plugin-preload-fonts',
-        'gatsby-plugin-pnpm',
         'gatsby-plugin-netlify-cms',
         {
             resolve: `gatsby-plugin-google-gtag`,
